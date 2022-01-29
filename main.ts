7b84a484838c76914b5401f54547dae39305054f
Filipko <98056125+FilipKopecny@users.noreply.github.com>
2022-01-29 22:16:43 +0100

    Aktualizovať main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,19 @@
 input.onSound(DetectedSound.Loud, function () {
+    basic.showIcon(IconNames.Yes)
+    basic.pause(200)
     pocet_tlesknuti += 1
+    basic.clearScreen()
 })
 let pocet_tlesknuti = 0
 input.setSoundThreshold(SoundThreshold.Loud, 140)
 basic.forever(function () {
-    basic.showNumber(pocet_tlesknuti)
     if (pocet_tlesknuti == 1) {
         if (Rover.Ultrasonic() > 30) {
-            Rover.MotorRunDual(200, 200)
+            Rover.MotorRunDual(115, 115)
         } else {
             Rover.MotorRunDual(100, -200)
         }
     } else {
         Rover.MotorStopAll(MotorActions.Stop)
     }
-    basic.pause(200)
 })
